98ba51bb1df878d4415671eddb706834fff22e9e
Hazarath <hazarathayyayallanki@gmail.com>
2024-07-30 02:50:40 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import Signin from './pages/SignIn'
 
 export default function App() {
-  return (
-    <h1 className='text-red-600'>App</h1>
-  )
+  return <BrowserRouter> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Sign-in" element={<Signin />} />
+        <Route path="/Sign-up" element={<SignUp />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+      </BrowserRouter>
+  
 }
